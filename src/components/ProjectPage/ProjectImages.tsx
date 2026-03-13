b4ffc2f1c4cfa1609f28fc2d2b1e2c63627fb86f
Eden <eden.vandewatyne@estiam.com>
2026-03-13 22:12:10 +0100

    Amelioration de ProjectImages, déplacement avec touche

--- src/components/ProjectPage/ProjectImages.tsx
+++ src/components/ProjectPage/ProjectImages.tsx
@@ -1,13 +1,43 @@
 import type { ProjectImagesProps } from "@/types"
+import { useRef, type KeyboardEvent } from "react"
 
 const ProjectImages = ({ images }: ProjectImagesProps) => {
+    const sliderRef = useRef<HTMLDivElement>(null)
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.repeat) {
+            return
+        }
+
+        if (!sliderRef.current) {
+            return
+        }
+
+        const container = sliderRef.current
+        const slideWidth = container.clientWidth
+
+        if (event.key == "ArrowRight") {
+            container.scrollBy({ left: slideWidth, behavior: "smooth" })
+        }
+
+        if (event.key == "ArrowLeft") {
+            container.scrollBy({ left: -slideWidth, behavior: "smooth" })
+        }
+    }
+
     return (
         <div className="px-12 md:px-20 lg:px-28 pb-16">
             <h2 className="text-2xl md:text-3xl font-bold text-lime-400 mb-6">
                 Galerie d'images
             </h2>
 
-            <div className="flex overflow-x-auto pb-8 snap-x snap-mandatory scrollbar-hide">
+            {/* TODO: MaJ pour déplacer avec la souris */}
+            <div
+                ref={sliderRef}
+                tabIndex={0}
+                onKeyDown={handleKeyDown}
+                className="flex overflow-x-auto pb-8 snap-x snap-mandatory scrollbar-hide outline-none focus:outline-none"
+            >
                 {images.map((image) => (
                     <div className="min-w-full snap-center px-2">
                         <img
